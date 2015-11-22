d18dbbe7e7cbed1a46a5d5499a2eb31089cbd360
Christopher Chan <cac2161@columbia.edu>
2015-11-22 03:01:43 -0500

    Add more skeleton code (#8 #6)

--- app/Search/Search.php
+++ app/Search/Search.php
@@ -1,21 +1,105 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: cc
- * Date: 11/22/15
- * Time: 1:36 AM
- */
 
 namespace App\Search;
 
-
 use Elasticsearch\ClientBuilder;
+use Elasticsearch\Client;
 
 class Search
 {
+    /** @var Client */
+    private $client;
 
     public function __construct()
     {
-        $client = ClientBuilder::create()->setHosts([env('ES_HOST')])->build();
+        $this->client = ClientBuilder::create()
+            ->setHosts([env('ES_HOST')])
+            ->setRetries(3)
+            ->build();
+    }
+
+    /**
+     * Create the index.
+     */
+    public function createIndex()
+    {
+
+        $create = [
+            'index' => 'my_index',
+            'body' => [
+                'settings' => [
+                    'number_of_shards' => 3,
+                    'number_of_replicas' => 2
+                ],
+                'mappings' => [
+                    'my_type' => [
+                        '_source' => [
+                            'enabled' => true,
+                            'properties' => [
+                                'first_name' => [
+                                    'type' => 'string',
+                                    'analyzer' => 'standard'
+                                ],
+                                'age' => [
+                                    'type' => 'integer'
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+
+    }
+
+    /**
+     * Get the schema.
+     *
+     * @return array
+     */
+    public function getSchema()
+    {
+
+        return [];
+    }
+
+    /**
+     * Faceted search
+     *
+     * @param array $params
+     */
+    public function search(array $params)
+    {
+
+    }
+
+    /**
+     * Fast search for autocomplete. Only search specific fields.
+     *
+     * @param $text
+     */
+    public function searchAutocomplete($text)
+    {
+
+    }
+
+    /**
+     * Insert a document.
+     *
+     * @param array $doc
+     */
+    public function insertDocument(array $doc)
+    {
+
+    }
+
+    /**
+     * Insert an array of documents.
+     *
+     * @param array $docs
+     */
+    public function insertDocuments(array $docs)
+    {
+
     }
 }

18bb16291f2c3d8a0baf36567b2f17dc221b05d1
Ethan Hann <ethanhann@gmail.com>
2015-11-21 19:31:53 -0500

    WIP Connect frontend to stub API

--- app/Http/Controllers/Api/SearchStubController.php
+++ app/Http/Controllers/Api/SearchStubController.php
@@ -10,14 +10,16 @@ use App\Http\Controllers\Controller;
 
 class SearchStubController extends Controller
 {
-    public function getIndex()
+    public function getIndex(Request $request)
     {
+        $query = $request->input('q', '');
         return ApiResponseFactory::MakeEnvelope([
-            ['id' => 1, 'name' => 'Agumon'],
-            ['id' => 2, 'name' => 'Baalmon'],
-            ['id' => 3, 'name' => 'Baromon'],
-            ['id' => 4, 'name' => 'Gaomon'],
-            ['id' => 5, 'name' => 'Grademon'],
+            ['id' => 1, 'name' => 'Eusthenopteron foordi', 'thumbnailUrl' => 'http://bone-explorer.dev:3000/img/eusthenopteron_foordi.jpg', 'description' => 'blah blah blah'],
+            ['id' => 2, 'name' => 'Iridotriton hechti', 'thumbnailUrl' => 'http://bone-explorer.dev:3000/img/iridotriton_hechti.jpg', 'description' => 'blah blah blah'],
+            ['id' => 3, 'name' => 'Sipalocyon sp', 'thumbnailUrl' => 'http://bone-explorer.dev:3000/img/sipalocyon_sp.jpg', 'description' => 'blah blah blah'],
+            ['id' => 4, 'name' => 'Teinolophos trusleri', 'thumbnailUrl' => 'http://bone-explorer.dev:3000/img/teinolophos_trusleri.jpg', 'description' => 'blah blah blah'],
+        ], [
+            'q' => $query
         ]);
     }
 }
